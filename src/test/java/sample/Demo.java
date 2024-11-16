5e7afa67172cab75b20512e98aa021db92b02d94
davey <davey@davidsheela>
2024-11-16 09:34:37 +0530

    This is my first commit

---
+++ src/test/java/sample/Demo.java
@@ -0,0 +1,15 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+public void sample() {
+		
+		System.out.println("this is first commit");
+		System.out.println("second");
+		
+	}
+
+}
